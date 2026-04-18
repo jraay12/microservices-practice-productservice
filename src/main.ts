472a9b2c1bd27103ba1d88509da0b47d464805fe
John Ray <johnraycaete@gmail.com>
2026-04-18 12:17:09 +0800

    register the stock routes

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import ProductRoute from "./interfaces/routes/product.route";
 import { errorHandler } from "./shared/error/GlobalError";
+import StockRoute from "./interfaces/routes/stock.routes";
+
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
 
 app.use("/product", ProductRoute);
+app.use("/stock", StockRoute);
 
 app.use(errorHandler);
 
