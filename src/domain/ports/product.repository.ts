5e46405732bbf73c8046d7639bf0db0da8eae577
John Ray <johnraycaete@gmail.com>
2026-04-18 12:18:07 +0800

    add new interface findById

--- src/domain/ports/product.repository.ts
+++ src/domain/ports/product.repository.ts
@@ -3,4 +3,5 @@ import { Product } from "../entities/product.entity";
 export interface ProductRepository {
   create(product: Product): Promise<Product>;
   findBySku(sku: string): Promise<Product | null>
+  findById(id: string): Promise<Product | null>
 }
